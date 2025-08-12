5085ea3b9cca317d272dc7f7212a58f0d1809e5a
STENIO DUARTE MATOS COSTA - SF53166 <stenio.duarte@external.stellantis.com>
2025-08-12 14:49:26 -0300

    aula 2

---
+++ Curso pietro/Debug/Debugging.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int arcg, char *argv[]) {
+
+    printf("Mensagem 1.\n");
+    printf("Mensagem 2.\n");
+    printf("Mensagem 3.\n");
+    printf("Mensagem 4.\n");
+    printf("Mensagem 5.\n");
+
+
+    return 0;
+}
--- Curso pietro/helloworld.c
+++ Curso pietro/helloworld.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //código
+    printf("Hello, world!\n");
+
+    system("pause");
+
+}
