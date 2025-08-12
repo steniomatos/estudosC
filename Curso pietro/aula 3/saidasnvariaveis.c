28e69590c4d0cc5921449b6ac2156d1ba451c47e
STENIO DUARTE MATOS COSTA - SF53166 <stenio.duarte@external.stellantis.com>
2025-08-12 15:05:41 -0300

    aula 3

---
+++ Curso pietro/aula 3/saidasnvariaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    printf("Olá, tenho 50 anos e programol.\n");
+
+    printf("Valor inteiro: %d \n",10);
+    printf("valor real: %f\n",3.14159265);
+    printf("Valor real com 2 casas: %.2f \n",314159265);
+    printf("Dado de texto (caractere) %c \n", 'a');
+    printf("Dado de texto (string): %s \n ", "string");
+
+    return 0;
+}
